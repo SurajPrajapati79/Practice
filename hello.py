fc4d4e8e893a87c0e68eb6c6347afcba6a2b4eac
Suraj <suraj.prajapati@altysys.com>
2025-06-27 21:07:15 +0530

    Add duplicate print statements for user interaction

--- hello.py
+++ hello.py
@@ -1,3 +1,6 @@
 print("hello World! happy to see you")
 print("I am fine what about you?")
+
+print("I am fine what about you?")
+print("I am fine what about you?")
 print("This is a new line added to the code.")
